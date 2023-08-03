267ff237fa396653051d1c7aa91f47ec9ef58502
Alivia <43556891+17anguyen@users.noreply.github.com>
2023-08-03 16:37:55 -0700

    first commit

---
+++ src/components/components/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+// import { Tilt } from "react-tilt";
+const About = () => {
+  return (
+    <div>About</div>
+  )
+}
+
+export default About
---
+++ src/components/components/Works.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+// import { Tilt } from "react-tilt";
+const Works = () => {
+  return (
+    <div>Works</div>
+  )
+}
+
+export default Works
