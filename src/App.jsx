fb860c399b46af8baed90ffd9580d9406c27985a
Alivia <43556891+17anguyen@users.noreply.github.com>
2023-08-04 03:32:00 -0700

    fixed white screen bug and duplicate folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components/components';
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 
 
 
