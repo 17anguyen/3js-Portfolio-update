a4feca46e77247ada32590f1e8c5402f23a57685
Alivia <43556891+17anguyen@users.noreply.github.com>
2023-08-16 04:03:24 -0700

    fixed pixed gap

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
 
     return (
         <div
-            className='w-full flex bottom-0'
+            className='w-full flex bottom-0 pb-[10px]'
         >
             <div className='w-full flex  justify-evenly'>
                 <ul className='list-none hidden sm:flex flex-row gap-10'>
@@ -20,13 +20,13 @@ const Footer = () => {
                             key={footer.name}
                             className={`${active === footer.name ? "text-white" : "text-[#69005A]"
                                 } hover:text-white text-[18px] font-medium cursor-pointer`}
-                            onClick={() => setActive(footer.title)}
+                            onClick={() => setActive(footer.name)}
                         >
                             <a href={`#${footer.URL}`}>
                                 <img
                                     src={footer.icon}
                                     alt={footer.name}
-                                    className="w-auto h-[40px]" />
+                                    className="w-auto h-[50px]" />
                             </a>
                         </li>
                     ))}
@@ -35,7 +35,7 @@ const Footer = () => {
                             <img
                                 src={resume}
                                 alt="resume"
-                                className="w-auto h-[40px]" />
+                                className="w-auto h-[50px]" />
                         </a>
 
                     </li>
